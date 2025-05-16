064b8e4905761ffb969ca7d4d891b365f88c6bed
ByeongSu Kang <admin@cenox.co>
2025-05-16 16:56:50 +0900

    Testing using .a not .framework for universal binary

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,58 +4,76 @@ FFMPEG_VERSION=7.1.1
 FFMPEG_SOURCE_DIR=FFmpeg-n$FFMPEG_VERSION
 FFMPEG_LIBS="libavcodec libavdevice libavfilter libavformat libavutil libswresample libswscale"
 PREFIX=`pwd`/output
-ARCH=arm64
+ARCHS=("arm64" "x86_64")
 PLATFORMS=("iphoneos" "iphonesimulator" "macosx")
 
-if [ ! -d $FFMPEG_SOURCE_DIR ]; then
-  echo "Start downloading FFmpeg..."
-  curl -LJO https://codeload.github.com/FFmpeg/FFmpeg/tar.gz/n$FFMPEG_VERSION || exit 1
-  tar -zxvf FFmpeg-n$FFMPEG_VERSION.tar.gz || exit 1
-  rm -f FFmpeg-n$FFMPEG_VERSION.tar.gz
-fi
+# if [ ! -d $FFMPEG_SOURCE_DIR ]; then
+#   echo "Start downloading FFmpeg..."
+#   curl -LJO https://codeload.github.com/FFmpeg/FFmpeg/tar.gz/n$FFMPEG_VERSION || exit 1
+#   tar -zxvf FFmpeg-n$FFMPEG_VERSION.tar.gz || exit 1
+#   rm -f FFmpeg-n$FFMPEG_VERSION.tar.gz
+# fi
 
-echo "Start compiling FFmpeg..."
+# echo "Start compiling FFmpeg..."
 
-rm -rf $PREFIX
-mkdir -p $PREFIX
+# rm -rf $PREFIX
+# mkdir -p $PREFIX
 
-for PLATFORM in "${PLATFORMS[@]}"; do
-  if [ "$PLATFORM" == "macosx" ]; then
-    SYSROOT=$(xcrun --sdk macosx --show-sdk-path)
-    OS_FLAGS="-mmacosx-version-min=11.0"
-  elif [ "$PLATFORM" == "iphonesimulator" ]; then
-    SYSROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
-    OS_FLAGS="-mios-simulator-version-min=18.0"
-  else
-    SYSROOT=$(xcrun --sdk $PLATFORM --show-sdk-path)
-    OS_FLAGS="-mios-version-min=18.0"
-  fi
-  CC=$(xcrun -sdk $PLATFORM -f clang)
-  TARGET_PREFIX=$PREFIX/$PLATFORM-$ARCH
+# for PLATFORM in "${PLATFORMS[@]}"; do
+#   for ARCH in "${ARCHS[@]}"; do
 
-  echo "===== Compiling for $PLATFORM ($ARCH) ====="
+#     # no need to build x86_64 for iPhoneOS
+#     if [[ "$PLATFORM" == "iphoneos" && "$ARCH" == "x86_64" ]]; then continue; fi
 
-  cd $FFMPEG_SOURCE_DIR
+#     echo "Building for $PLATFORM - $ARCH"
 
-  ./configure \
-    --prefix=$TARGET_PREFIX \
-    --enable-version3 \
-    --disable-programs \
-    --disable-doc \
-    --disable-debug \
-    --disable-videotoolbox \
-    --enable-cross-compile \
-    --target-os=darwin \
-    --arch=$ARCH \
-    --cc="$CC" \
-    --sysroot=$SYSROOT \
-    --extra-cflags="-arch $ARCH -march=native -fno-stack-check $OS_FLAGS" \
-    --extra-ldflags="-arch $ARCH $OS_FLAGS" \
-    --disable-outdev=audiotoolbox || exit 1
+#     if [ "$PLATFORM" == "macosx" ]; then
+#       SYSROOT=$(xcrun --sdk macosx --show-sdk-path)
+#       OS_FLAGS="-mmacosx-version-min=11.0"
+#     elif [ "$PLATFORM" == "iphonesimulator" ]; then
+#       SYSROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+#       OS_FLAGS="-mios-simulator-version-min=18.0"
+#     else
+#       SYSROOT=$(xcrun --sdk iphoneos --show-sdk-path)
+#       OS_FLAGS="-mios-version-min=18.0"
+#     fi
 
-  make clean
-  make -j$(nproc) install || exit 1
-  cd ..
+#     TARGET_PREFIX="$PREFIX/$PLATFORM-$ARCH"
+#     CC=$(xcrun -sdk $PLATFORM -f clang)
+
+#     cd $FFMPEG_SOURCE_DIR
+#     make clean
+
+#     ./configure \
+#       --prefix=$TARGET_PREFIX \
+#       --enable-cross-compile \
+#       --target-os=darwin \
+#       --arch=$ARCH \
+#       --cc="$CC" \
+#       --sysroot=$SYSROOT \
+#       --extra-cflags="-arch $ARCH $OS_FLAGS" \
+#       --extra-ldflags="-arch $ARCH $OS_FLAGS" \
+#       --disable-programs \
+#       --disable-doc \
+#       --disable-debug \
+#       --enable-version3 \
+#       --disable-outdev=audiotoolbox \
+#       --disable-videotoolbox || exit 1
+
+#     make -j$(sysctl -n hw.logicalcpu) install || exit 1
+#     cd ..
+#   done
+# done
+
+for LIB in $FFMPEG_LIBS; do
+  for PLATFORM in "iphonesimulator" "macosx"; do
+    echo "Creating fat binary for $LIB ($PLATFORM)"
+    mkdir -p "$PREFIX/$PLATFORM-universal/lib"
+    lipo -create \
+      "$PREFIX/$PLATFORM-arm64/lib/$LIB.a" \
+      "$PREFIX/$PLATFORM-x86_64/lib/$LIB.a" \
+      -output "$PREFIX/$PLATFORM-universal/lib/$LIB.a"
+  done
 done
 
 for LIB in $FFMPEG_LIBS; do
--- Scripts/build_framework.sh
+++ Scripts/build_framework.sh
@@ -1,158 +1,179 @@
 #!/bin/bash
 
+set -e
+
 PREFIX=$1
 LIB_NAME=$2
 LIB_VERSION=$3
-PLATFORMS=("ios" "iphonesimulator" "macosx")
+
 LIB_XCFRAMEWORK=$PREFIX/xcframework/$LIB_NAME.xcframework
 
-for PLATFORM in "${PLATFORMS[@]}"; do
-	FRAMEWORK_PARENT_DIR=$PREFIX/framework/$PLATFORM
-
-	if [[ "$PLATFORM" == "ios" ]]; then
-		SDK_PLATFORM="iphoneos-arm64"
-		TARGET="iPhoneOS"
-	elif [[ "$PLATFORM" == "iphonesimulator" ]]; then
-		SDK_PLATFORM="iphonesimulator-arm64"
-		TARGET="iPhoneSimulator"
-	elif [[ "$PLATFORM" == "macosx" ]]; then
-		SDK_PLATFORM="macosx-arm64"
-		TARGET="MacOSX"
-	else
-		echo "❌ Unknown platform: $PLATFORM"
-		exit 1
-	fi
-
-	LIB_FRAMEWORK=$FRAMEWORK_PARENT_DIR/$LIB_NAME.framework
-
-	# save xcframework input variable
-	if [[ "$PLATFORM" == "ios" ]]; then
-		LIB_DEVICE=$LIB_FRAMEWORK
-	elif [[ "$PLATFORM" == "iphonesimulator" ]]; then
-		LIB_SIM=$LIB_FRAMEWORK
-	else
-		LIB_MACOS=$LIB_FRAMEWORK
-	fi
-
-	# build framework
-	rm -rf $LIB_FRAMEWORK
-	mkdir -p $LIB_FRAMEWORK/Headers
-
-	cp -R $PREFIX/$SDK_PLATFORM/include/$LIB_NAME/* $LIB_FRAMEWORK/Headers/
-	libtool -static -o $LIB_FRAMEWORK/$LIB_NAME $PREFIX/$SDK_PLATFORM/lib/$LIB_NAME.a
-
-	cat > $LIB_FRAMEWORK/Info.plist << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>CFBundleDevelopmentRegion</key>
-	<string>en</string>
-	<key>CFBundleExecutable</key>
-	<string>$LIB_NAME</string>
-	<key>CFBundleIdentifier</key>
-	<string>org.ffmpeg.$LIB_NAME</string>
-	<key>CFBundleInfoDictionaryVersion</key>
-	<string>6.0</string>
-	<key>CFBundleName</key>
-	<string>$LIB_NAME</string>
-	<key>CFBundlePackageType</key>
-	<string>FMWK</string>
-	<key>CFBundleShortVersionString</key>
-	<string>$LIB_VERSION</string>
-	<key>CFBundleVersion</key>
-	<string>$LIB_VERSION</string>
-	<key>CFBundleSupportedPlatforms</key>
-	<array>
-		<string>$TARGET</string>
-	</array>
-</dict>
-</plist>
-EOF
-
-done
-
-# build xcframework
 rm -rf $LIB_XCFRAMEWORK
+mkdir -p $(dirname "$LIB_XCFRAMEWORK")
 
-xcodebuild \
-	-verbose \
-	-create-xcframework \
-	-framework $LIB_DEVICE \
-	-framework $LIB_SIM \
-	-framework $LIB_MACOS \
-	-output $LIB_XCFRAMEWORK
+xcodebuild -create-xcframework \
+  -library $PREFIX/iphoneos-arm64/lib/$LIB_NAME.a -headers $PREFIX/iphoneos-arm64/include/$LIB_NAME \
+  -library $PREFIX/iphonesimulator-universal/lib/$LIB_NAME.a -headers $PREFIX/iphonesimulator-arm64/include/$LIB_NAME \
+  -library $PREFIX/macosx-universal/lib/$LIB_NAME.a -headers $PREFIX/macosx-arm64/include/$LIB_NAME \
+  -output $LIB_XCFRAMEWORK
 
-# error: unable to find any specific architecture information in the binary at xxx
+echo "Successfully created: $LIB_XCFRAMEWORK"
 
-mkdir -p $LIB_XCFRAMEWORK/ios-arm64
-mkdir -p $LIB_XCFRAMEWORK/iossim-arm64
-mkdir -p $LIB_XCFRAMEWORK/macos-arm64
+# PREFIX=$1
+# LIB_NAME=$2
+# LIB_VERSION=$3
+# PLATFORMS=("ios" "iphonesimulator" "macosx")
+# LIB_XCFRAMEWORK=$PREFIX/xcframework/$LIB_NAME.xcframework
 
-cp -R $LIB_DEVICE $LIB_XCFRAMEWORK/ios-arm64
-cp -R $LIB_SIM $LIB_XCFRAMEWORK/iossim-arm64
-cp -R $LIB_MACOS $LIB_XCFRAMEWORK/macos-arm64
+# for PLATFORM in "${PLATFORMS[@]}"; do
+# 	FRAMEWORK_PARENT_DIR=$PREFIX/framework/$PLATFORM
 
-cat > $LIB_XCFRAMEWORK/Info.plist << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>AvailableLibraries</key>
-	<array>
-		<dict>
-			<key>LibraryIdentifier</key>
-			<string>ios-arm64</string>
-			<key>LibraryPath</key>
-			<string>$LIB_NAME.framework</string>
-			<key>SupportedArchitectures</key>
-			<array>
-				<string>arm64</string>
-			</array>
-			<key>SupportedPlatform</key>
-			<string>ios</string>
-		</dict>
-		<dict>
-			<key>LibraryIdentifier</key>
-			<string>ios-arm64-simulator</string>
-			<key>LibraryPath</key>
-			<string>$LIB_NAME.framework</string>
-			<key>SupportedArchitectures</key>
-			<array>
-				<string>arm64</string>
-			</array>
-			<key>SupportedPlatform</key>
-			<string>iossimulator</string>
-		</dict>
-		<dict>
-			<key>LibraryIdentifier</key>
-			<string>iossim-arm64</string>
-			<key>LibraryPath</key>
-			<string>$LIB_NAME.framework</string>
-			<key>SupportedArchitectures</key>
-			<array>
-				<string>arm64</string>
-			</array>
-			<key>SupportedPlatform</key>
-			<string>iphonesimulator</string>
-		</dict>
-		<dict>
-			<key>LibraryIdentifier</key>
-			<string>macos-arm64</string>
-			<key>LibraryPath</key>
-			<string>$LIB_NAME.framework</string>
-			<key>SupportedArchitectures</key>
-			<array>
-				<string>arm64</string>
-			</array>
-			<key>SupportedPlatform</key>
-			<string>macos</string>
-		</dict>
-	</array>
-	<key>CFBundlePackageType</key>
-	<string>XFWK</string>
-	<key>XCFrameworkFormatVersion</key>
-	<string>1.0</string>
-</dict>
-</plist>
-EOF
+# 	if [[ "$PLATFORM" == "ios" ]]; then
+# 		SDK_PLATFORM="iphoneos-arm64"
+# 		TARGET="iPhoneOS"
+# 	elif [[ "$PLATFORM" == "iphonesimulator" ]]; then
+# 		SDK_PLATFORM="iphonesimulator-arm64"
+# 		TARGET="iPhoneSimulator"
+# 	elif [[ "$PLATFORM" == "macosx" ]]; then
+# 		SDK_PLATFORM="macosx-arm64"
+# 		TARGET="MacOSX"
+# 	else
+# 		echo "❌ Unknown platform: $PLATFORM"
+# 		exit 1
+# 	fi
+
+# 	LIB_FRAMEWORK=$FRAMEWORK_PARENT_DIR/$LIB_NAME.framework
+
+# 	# save xcframework input variable
+# 	if [[ "$PLATFORM" == "ios" ]]; then
+# 		LIB_DEVICE=$LIB_FRAMEWORK
+# 	elif [[ "$PLATFORM" == "iphonesimulator" ]]; then
+# 		LIB_SIM=$LIB_FRAMEWORK
+# 	else
+# 		LIB_MACOS=$LIB_FRAMEWORK
+# 	fi
+
+# 	# build framework
+# 	rm -rf $LIB_FRAMEWORK
+# 	mkdir -p $LIB_FRAMEWORK/Headers
+
+# 	cp -R $PREFIX/$SDK_PLATFORM/include/$LIB_NAME/* $LIB_FRAMEWORK/Headers/
+# 	libtool -static -o $LIB_FRAMEWORK/$LIB_NAME $PREFIX/$SDK_PLATFORM/lib/$LIB_NAME.a
+
+# 	cat > $LIB_FRAMEWORK/Info.plist << EOF
+# <?xml version="1.0" encoding="UTF-8"?>
+# <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+# <plist version="1.0">
+# <dict>
+# 	<key>CFBundleDevelopmentRegion</key>
+# 	<string>en</string>
+# 	<key>CFBundleExecutable</key>
+# 	<string>$LIB_NAME</string>
+# 	<key>CFBundleIdentifier</key>
+# 	<string>org.ffmpeg.$LIB_NAME</string>
+# 	<key>CFBundleInfoDictionaryVersion</key>
+# 	<string>6.0</string>
+# 	<key>CFBundleName</key>
+# 	<string>$LIB_NAME</string>
+# 	<key>CFBundlePackageType</key>
+# 	<string>FMWK</string>
+# 	<key>MinimumOSVersion</key>
+# 	<string>18.0</string>
+# 	<key>CFBundleShortVersionString</key>
+# 	<string>$LIB_VERSION</string>
+# 	<key>CFBundleVersion</key>
+# 	<string>$LIB_VERSION</string>
+# 	<key>CFBundleSupportedPlatforms</key>
+# 	<array>
+# 		<string>$TARGET</string>
+# 	</array>
+# </dict>
+# </plist>
+# EOF
+
+# done
+
+# # build xcframework
+# rm -rf $LIB_XCFRAMEWORK
+
+# xcodebuild \
+# 	-verbose \
+# 	-create-xcframework \
+# 	-framework $LIB_DEVICE \
+# 	-framework $LIB_SIM \
+# 	-framework $LIB_MACOS \
+# 	-output $LIB_XCFRAMEWORK
+
+# # error: unable to find any specific architecture information in the binary at xxx
+
+# mkdir -p $LIB_XCFRAMEWORK/ios-arm64
+# mkdir -p $LIB_XCFRAMEWORK/iossim-arm64
+# mkdir -p $LIB_XCFRAMEWORK/macos-arm64
+
+# cp -R $LIB_DEVICE $LIB_XCFRAMEWORK/ios-arm64
+# cp -R $LIB_SIM $LIB_XCFRAMEWORK/iossim-arm64
+# cp -R $LIB_MACOS $LIB_XCFRAMEWORK/macos-arm64
+
+# cat > $LIB_XCFRAMEWORK/Info.plist << EOF
+# <?xml version="1.0" encoding="UTF-8"?>
+# <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+# <plist version="1.0">
+# <dict>
+# 	<key>AvailableLibraries</key>
+# 	<array>
+# 		<dict>
+# 			<key>LibraryIdentifier</key>
+# 			<string>ios-arm64</string>
+# 			<key>LibraryPath</key>
+# 			<string>$LIB_NAME.framework</string>
+# 			<key>SupportedArchitectures</key>
+# 			<array>
+# 				<string>arm64</string>
+# 			</array>
+# 			<key>SupportedPlatform</key>
+# 			<string>ios</string>
+# 		</dict>
+# 		<dict>
+# 			<key>LibraryIdentifier</key>
+# 			<string>ios-arm64-simulator</string>
+# 			<key>LibraryPath</key>
+# 			<string>$LIB_NAME.framework</string>
+# 			<key>SupportedArchitectures</key>
+# 			<array>
+# 				<string>arm64</string>
+# 			</array>
+# 			<key>SupportedPlatform</key>
+# 			<string>iossimulator</string>
+# 		</dict>
+# 		<dict>
+# 			<key>LibraryIdentifier</key>
+# 			<string>ios-arm64-simulator</string>
+# 			<key>LibraryPath</key>
+# 			<string>$LIB_NAME.framework</string>
+# 			<key>SupportedArchitectures</key>
+# 			<array>
+# 				<string>arm64</string>
+# 			</array>
+# 			<key>SupportedPlatform</key>
+# 			<string>iphonesimulator</string>
+# 		</dict>
+# 		<dict>
+# 			<key>LibraryIdentifier</key>
+# 			<string>macos-arm64</string>
+# 			<key>LibraryPath</key>
+# 			<string>$LIB_NAME.framework</string>
+# 			<key>SupportedArchitectures</key>
+# 			<array>
+# 				<string>arm64</string>
+# 			</array>
+# 			<key>SupportedPlatform</key>
+# 			<string>macos</string>
+# 		</dict>
+# 	</array>
+# 	<key>CFBundlePackageType</key>
+# 	<string>XFWK</string>
+# 	<key>XCFrameworkFormatVersion</key>
+# 	<string>1.0</string>
+# </dict>
+# </plist>
+# EOF
